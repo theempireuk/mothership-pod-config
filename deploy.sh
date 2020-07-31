179387c65321b5b913fed6270b8d5d583c9f6e01
Fynn Hunt <fynnhunt@hotmail.co.uk>
2020-07-31 13:00:42 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ sudo docker stop mothership-pod
 sudo docker prune -f
 sudo docker pull theempire/mothership-pod:latest
 sudo docker system prune -f
-sudo docker run --env-file ./env.list -d --rm --name mothership-pod -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/mothership-pod/env.list:/home/ubuntu/mothership-pod/env.list theempire/mothership-pod
+sudo docker run --env-file ./env.list -d --rm -it --name mothership-pod -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/mothership-pod/env.list:/home/ubuntu/mothership-pod/env.list theempire/mothership-pod
