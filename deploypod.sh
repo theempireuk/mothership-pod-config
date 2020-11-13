3f6da81a6445de71a28fb9b771c7b7b007765c9c
Fynn <fynnhunt@hotmail.co.uk>
2020-11-13 16:09:18 +0000

    Fixed deploy pod

--- deploypod.sh
+++ deploypod.sh
@@ -2,4 +2,4 @@ sudo docker stop mothership-pod
 sudo docker prune -f
 sudo docker pull theempire/mothership-pod:latest
 sudo docker system prune -f
-docker run --env-file ./env.list --rm -it -d --name mothership-pod -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/mothership-pod-config/env.list:/home/ubuntu/mothership-pod-config/env.list theempire/mothership-pod
+docker run --env-file ./env.list --rm -it -d --name mothership-pod -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/mothership-pod-config/env.list:/home/ubuntu/mothership-pod-config/env.list -v $PWD:$PWD theempire/mothership-pod
